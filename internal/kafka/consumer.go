2feba59fdb945d42c54c9d5900c6d67fa7e055d2
матвей <mamkinhecker@Noutbuk-matvej.local>
2025-08-19 13:23:19 +0300

    kafka and handlers preparation

--- internal/kafka/consumer.go
+++ internal/kafka/consumer.go
@@ -1,1 +1,70 @@
 package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"order-service/config"
+	"order-service/internal/cache"
+	"order-service/internal/db"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Consumer struct {
+	reader *kafka.Reader
+	db     *db.Database
+	cache  *cache.Cache
+}
+
+func NewConsumer(cfg *config.Config, db *db.Database, cache *cache.Cache) *Consumer {
+	return &Consumer{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers:        cfg.KafkaBrokers,
+			Topic:          cfg.KafkaTopic,
+			GroupID:        "order-service",
+			MinBytes:       10e3,
+			MaxBytes:       10e6,
+			CommitInterval: time.Second,
+		}),
+		db:    db,
+		cache: cache}
+}
+
+func (c *Consumer) start() {
+	go c.ConsumeMessages()
+}
+
+func (c *Consumer) ConsumeMessages() {
+	for {
+		msg, err := c.reader.FetchMessage(context.Background())
+		if err != nil {
+			log.Printf("Failed to fetch message: %v", err)
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		var order db.Order
+		if err := json.Unmarshal(msg.Value, &order); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			c.reader.CommitMessages(context.Background(), msg)
+			continue
+		}
+
+		if err := c.db.SaveOrder(&order); err != nil {
+			log.Printf("Failed to save order to DB: %v", err)
+		} else {
+			c.cache.Set(&order)
+			log.Printf("Order %s saved and cached", order.OrderUID)
+		}
+
+		if err := c.reader.CommitMessages(context.Background(), msg); err != nil {
+			log.Printf("Failed to commit message: %v", err)
+		}
+	}
+}
+
+func (c *Consumer) Close() error {
+	return c.reader.Close()
+}
--- internal/web/handlers.go
+++ internal/web/handlers.go
@@ -1,1 +1,52 @@
 package web
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"order-service/internal/cache"
+	"order-service/internal/db"
+)
+
+type OrderHandler struct {
+	cache *cache.Cache
+	db    *db.Database
+}
+
+func NewOrderHandler(cache *cache.Cache, db *db.Database) *OrderHandler {
+	return &OrderHandler{cache, db}
+}
+
+func (h *OrderHandler) GetOrder(w http.ResponseWriter, r *http.Request) {
+	uid := r.URL.Path[len("/order/"):]
+	if uid == "" {
+		http.Error(w, "order UID is required", http.StatusBadRequest)
+		return
+	}
+
+	if order, ok := h.cache.Get(uid); ok {
+		respondWithJSON(w, http.StatusOK, order)
+		return
+	}
+
+	order, err := h.db.GetOrderByUID(uid)
+	if err != nil {
+		http.Error(w, "Order not found", http.StatusNotFound)
+		return
+	}
+
+	h.cache.Set(order)
+	respondWithJSON(w, http.StatusOK, order)
+}
+
+func respondWithJSON(w http.ResponseWriter, StatusCode int, data interface{}) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(StatusCode)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Printf("Failed to encode JSON response: %v", err)
+	}
+}
+
+func StaticHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./web/static/index.html")
+}
